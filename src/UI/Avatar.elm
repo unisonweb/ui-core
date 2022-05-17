0aa389197ce992148f832ea6a0bb35cd20cba330
Simon Højberg <r.hackr@gmail.com>
2022-05-17 12:57:46 -0400

    Add avatar builder pattern helper functions

--- src/UI/Avatar.elm
+++ src/UI/Avatar.elm
@@ -9,10 +9,52 @@ import Url exposing (Url)
 
 
 type alias Avatar msg =
-    { text : Maybe String
-    , url : Maybe Url
+    { url : Maybe Url
+    , text : Maybe String
     , fallbackIcon : Maybe (Icon msg)
     }
+
+
+
+-- CREATE
+
+
+empty : Avatar msg
+empty =
+    { url = Nothing, text = Nothing, fallbackIcon = Nothing }
+
+
+avatar : Maybe Url -> Maybe String -> Avatar msg
+avatar url text =
+    avatar_ url text (Just Icon.user)
+
+
+avatar_ : Maybe Url -> Maybe String -> Maybe (Icon msg) -> Avatar msg
+avatar_ url text fallbackIcon =
+    { url = url, text = text, fallbackIcon = fallbackIcon }
+
+
+
+-- MODIFY
+
+
+withUrl : Url -> Avatar msg -> Avatar msg
+withUrl url avatar__ =
+    { avatar__ | url = Just url }
+
+
+withText : String -> Avatar msg -> Avatar msg
+withText text avatar__ =
+    { avatar__ | text = Just text }
+
+
+withIcon : Icon msg -> Avatar msg -> Avatar msg
+withIcon fallbackIcon avatar__ =
+    { avatar__ | fallbackIcon = Just fallbackIcon }
+
+
+
+-- VIEW
 
 
 view : Avatar msg -> Html msg
