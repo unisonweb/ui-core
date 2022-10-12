c4fcad1eca1ac7babf7e9c9ca6996e6ac24ba7d1
Simon Højberg <r.hackr@gmail.com>
2022-10-12 18:38:54 -0400

    Debug tooltip

--- src/Code/DefinitionSummaryTooltip.elm
+++ src/Code/DefinitionSummaryTooltip.elm
@@ -64,7 +64,7 @@ update config msg model =
         FetchDefinitionFinished ref d ->
             case model of
                 Just ( r, _ ) ->
-                    if Reference.equals ref r then
+                    if Debug.log "eq" (Reference.equals ref r) then
                         ( Just ( r, RemoteData.fromResult d ), Cmd.none )
 
                     else
