1131381922985e8e77cde787a39bbb3a52875289
Simon Højberg <r.hackr@gmail.com>
2022-12-06 13:56:20 -0500

    Project listing

--- src/Code/ProjectListing.elm
+++ src/Code/ProjectListing.elm
@@ -4,24 +4,38 @@ import Code.Hash as Hash
 import Code.Hashvatar as Hashvatar
 import Code.Project exposing (Project)
 import Code.Project.ProjectShorthand as ProjectShorthand
-import Html exposing (Html)
+import Html exposing (Html, div)
 import Html.Attributes exposing (class)
-import UI.Click as Click
+import UI.Click as Click exposing (Click)
 
 
-type alias ProjectListing p =
+type alias ProjectListing p msg =
     { project : Project p
+    , click : Maybe (Click msg)
     }
 
 
-view : Click.Click msg -> ProjectListing p -> Html msg
-view click { project } =
+withClick : Click msg -> ProjectListing p msg -> ProjectListing p msg
+withClick click p =
+    { p | click = Just click }
+
+
+view : ProjectListing p msg -> Html msg
+view { project, click } =
     let
         hash =
             Hash.unsafeFromString (ProjectShorthand.toString project.shorthand)
     in
-    Click.view [ class "project-listing" ]
-        [ Hashvatar.view hash
-        , ProjectShorthand.view project.shorthand
-        ]
-        click
+    case click of
+        Just c ->
+            Click.view [ class "project-listing" ]
+                [ Hashvatar.view hash
+                , ProjectShorthand.view project.shorthand
+                ]
+                c
+
+        Nothing ->
+            div [ class "project-listing" ]
+                [ Hashvatar.view hash
+                , ProjectShorthand.view project.shorthand
+                ]
