30e5a4f61f22b2326b219787945985413962737f
Simon Højberg <r.hackr@gmail.com>
2022-12-12 14:29:00 -0500

    Add large project listing

--- src/Code/ProjectListing.elm
+++ src/Code/ProjectListing.elm
@@ -9,15 +9,65 @@ import Html.Attributes exposing (class)
 import UI.Click as Click exposing (Click)
 
 
+type ProjectListingSize
+    = Medium
+    | Large
+
+
 type alias ProjectListing p msg =
     { project : Project p
     , click : Maybe (Click msg)
+    , size : ProjectListingSize
     }
+
+
+
+-- CREATE
+
+
+projectListing : Project p -> ProjectListing p msg
+projectListing project =
+    { project = project, click = Nothing, size = Medium }
+
+
+
+-- MODIFY
 
 
 withClick : Click msg -> ProjectListing p msg -> ProjectListing p msg
 withClick click p =
     { p | click = Just click }
+
+
+withSize : ProjectListingSize -> ProjectListing p msg -> ProjectListing p msg
+withSize size p =
+    { p | size = size }
+
+
+medium : ProjectListing p msg -> ProjectListing p msg
+medium p =
+    withSize Medium p
+
+
+large : ProjectListing p msg -> ProjectListing p msg
+large p =
+    withSize Large p
+
+
+
+-- MAP
+
+
+map : (aMsg -> bMsg) -> ProjectListing p aMsg -> ProjectListing p bMsg
+map f p =
+    { project = p.project
+    , click = Maybe.map (Click.map f) p.click
+    , size = p.size
+    }
+
+
+
+-- VIEW
 
 
 view : ProjectListing p msg -> Html msg
