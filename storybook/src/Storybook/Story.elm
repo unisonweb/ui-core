36c862aa1107a73fa062577190384dbcf44e4e20
kentookura <kento@okura.at>
2022-12-30 16:04:46 +0100

    add logAction

--- storybook/src/Storybook/Story.elm
+++ storybook/src/Storybook/Story.elm
@@ -1,7 +1,10 @@
-module Storybook.Story exposing (Story, sandbox, stateless)
+port module Storybook.Story exposing (Story, sandbox, stateless)
 
 import Browser
 import Html exposing (Html)
+
+
+port logAction : String -> Cmd msg
 
 
 type alias Story model msg =
@@ -12,7 +15,7 @@ stateless : { view : Html msg } -> Story () msg
 stateless options =
     Browser.element
         { init = \() -> ( (), Cmd.none )
-        , update = \msg model -> ( model, Cmd.none )
+        , update = \msg model -> ( model, logAction (Debug.toString msg) )
         , view = \_ -> options.view
         , subscriptions = \_ -> Sub.none
         }
@@ -27,7 +30,7 @@ sandbox :
 sandbox options =
     Browser.element
         { init = \() -> ( options.init, Cmd.none )
-        , update = \msg model -> ( options.update msg model, Cmd.none )
+        , update = \msg model -> ( options.update msg model, logAction (Debug.toString msg) )
         , view = \model -> options.view model
         , subscriptions = \_ -> Sub.none
         }
