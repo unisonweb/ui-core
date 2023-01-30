a05373383aaa8b1f91775ce73f023ffa8d6d05eb
Simon Højberg <r.hackr@gmail.com>
2023-01-30 16:04:17 -0500

    prefer title over heading

--- src/UI/PageContent.elm
+++ src/UI/PageContent.elm
@@ -20,14 +20,14 @@ import UI.Icon as Icon exposing (Icon)
 
 type alias PageTitle msg =
     { icon : Maybe (Icon msg)
-    , heading : String
+    , title : String
     , description : Maybe String
     }
 
 
 type PageContent msg
     = PageContent
-        { heading : Maybe (PageTitle msg)
+        { title : Maybe (PageTitle msg)
         , content : List (List (Html msg))
         }
 
@@ -38,28 +38,28 @@ type PageContent msg
 
 empty : PageContent msg
 empty =
-    PageContent { heading = Nothing, content = [] }
+    PageContent { title = Nothing, content = [] }
 
 
-{-| Create a page content with a single column and no heading
+{-| Create a page content with a single column and no title
 -}
 oneColumn : List (Html msg) -> PageContent msg
 oneColumn rows =
-    PageContent { heading = Nothing, content = [ rows ] }
+    PageContent { title = Nothing, content = [ rows ] }
 
 
-{-| Create a page content with 2 columns and no heading
+{-| Create a page content with 2 columns and no title
 -}
 twoColumns : ( List (Html msg), List (Html msg) ) -> PageContent msg
 twoColumns ( one, two ) =
-    PageContent { heading = Nothing, content = [ one, two ] }
+    PageContent { title = Nothing, content = [ one, two ] }
 
 
-{-| Create a page content with 3 columns and no heading
+{-| Create a page content with 3 columns and no title
 -}
 threeColumns : ( List (Html msg), List (Html msg), List (Html msg) ) -> PageContent msg
 threeColumns ( one, two, three ) =
-    PageContent { heading = Nothing, content = [ one, two, three ] }
+    PageContent { title = Nothing, content = [ one, two, three ] }
 
 
 
@@ -70,7 +70,7 @@ threeColumns ( one, two, three ) =
 -}
 withPageTitle : PageTitle msg -> PageContent msg -> PageContent msg
 withPageTitle pageTitle (PageContent cfg) =
-    PageContent { cfg | heading = Just pageTitle }
+    PageContent { cfg | title = Just pageTitle }
 
 
 
@@ -80,7 +80,7 @@ withPageTitle pageTitle (PageContent cfg) =
 map : (a -> msg) -> PageContent a -> PageContent msg
 map toMsg (PageContent pageContentA) =
     PageContent
-        { heading = Maybe.map (mapPageTitle toMsg) pageContentA.heading
+        { title = Maybe.map (mapPageTitle toMsg) pageContentA.title
         , content = List.map (List.map (Html.map toMsg)) pageContentA.content
         }
 
@@ -88,7 +88,7 @@ map toMsg (PageContent pageContentA) =
 mapPageTitle : (a -> msg) -> PageTitle a -> PageTitle msg
 mapPageTitle toMsg pageTitleA =
     { icon = Maybe.map (Icon.map toMsg) pageTitleA.icon
-    , heading = pageTitleA.heading
+    , title = pageTitleA.title
     , description = pageTitleA.description
     }
 
@@ -98,34 +98,34 @@ mapPageTitle toMsg pageTitleA =
 
 
 viewPageTitle : PageTitle msg -> Html msg
-viewPageTitle { icon, heading, description } =
+viewPageTitle { icon, title, description } =
     let
         items =
             case ( icon, description ) of
                 ( Nothing, Nothing ) ->
-                    [ h1 [] [ text heading ] ]
+                    [ h1 [] [ text title ] ]
 
                 ( Nothing, Just d ) ->
                     [ div [ class "text" ]
-                        [ h1 [] [ text heading ]
+                        [ h1 [] [ text title ]
                         , p [ class "description" ] [ text d ]
                         ]
                     ]
 
                 ( Just i, Nothing ) ->
                     [ div [ class "icon-badge" ] [ Icon.view i ]
-                    , h1 [] [ text heading ]
+                    , h1 [] [ text title ]
                     ]
 
                 ( Just i, Just d ) ->
                     [ div [ class "icon-badge" ] [ Icon.view i ]
                     , div [ class "text" ]
-                        [ h1 [] [ text heading ]
+                        [ h1 [] [ text title ]
                         , p [ class "description" ] [ text d ]
                         ]
                     ]
     in
-    header [ class "page-heading" ] items
+    header [ class "page-title" ] items
 
 
 viewColumn : List (Html msg) -> Html msg
@@ -144,10 +144,10 @@ view pageContent =
 
 
 view_ : Html msg -> PageContent msg -> Html msg
-view_ footer (PageContent { heading, content }) =
+view_ footer (PageContent { title, content }) =
     let
         items =
-            case heading of
+            case title of
                 Just h ->
                     [ viewPageTitle h, viewColumns content, footer ]
 
