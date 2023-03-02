6f8b01429c15ed9fb2930cecb45606dbbac42e6b
Simon Højberg <r.hackr@gmail.com>
2023-03-02 12:54:59 -0500

    Fix scroll to

--- src/Lib/ScrollTo.elm
+++ src/Lib/ScrollTo.elm
@@ -13,7 +13,7 @@ scrollTo doneMsg containerId targetId =
         ]
         |> Task.andThen
             (\ys ->
-                case Debug.log "elY, viewportY, viewportscrollTop" ys of
+                case ys of
                     elY :: viewportY :: viewportScrollTop :: [] ->
                         Dom.setViewportOf containerId 0 (viewportScrollTop + (elY - viewportY))
                             |> Task.onError (\_ -> Task.succeed ())
@@ -21,4 +21,4 @@ scrollTo doneMsg containerId targetId =
                     _ ->
                         Task.succeed ()
             )
-        |> Task.attempt (Debug.log "result" >> always doneMsg)
+        |> Task.attempt (always doneMsg)
