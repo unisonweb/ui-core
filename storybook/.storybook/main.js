a2d83f6346b288ad511447c7ad5b9322f62560a9
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-03-10 12:28:34 +0100

    Update to webpack5

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,12 +1,8 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/html"
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": "@storybook/html",
+  core: {
+    builder: "webpack5"
+  }
+};
