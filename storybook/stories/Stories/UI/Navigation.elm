396de603b805345a916c9c49cd5f6963b9ead81e
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-04-01 21:28:18 +0200

    Update to use List in Navigation

--- storybook/stories/Stories/UI/Navigation.elm
+++ storybook/stories/Stories/UI/Navigation.elm
@@ -1,6 +1,5 @@
 module Stories.UI.Navigation exposing (..)
 
-import Array
 import Browser
 import Helpers.Layout exposing (col)
 import Html exposing (Html)
@@ -30,49 +29,37 @@ sampleNavItem =
     N.navItem "some" (C.ExternalHref "some")
 
 
-sampleNavItems : Array.Array (N.NavItem Msg)
+sampleNavItems : List (N.NavItem Msg)
 sampleNavItems =
     [ C.ExternalHref "link 1" |> N.navItem "item 1"
     , C.ExternalHref "link 2" |> N.navItem "item 2"
     , C.ExternalHref "link 3" |> N.navItem "item 3"
     , C.ExternalHref "link 4" |> N.navItem "item 4"
     ]
-        |> Array.fromList
 
 
 elements : List (N.Navigation Msg)
 elements =
     [ N.withNoSelectedItems
-        (sampleNavItems
-            |> Array.toList
-        )
+        sampleNavItems
         N.empty
     , N.withItems
-        (Array.slice 0 2 sampleNavItems
-            |> Array.toList
-        )
-        (Array.get 2 sampleNavItems
-            |> Maybe.withDefault sampleNavItem
-        )
-        (Array.slice 3 4 sampleNavItems
-            |> Array.toList
-        )
+        (List.take 2 sampleNavItems)
+        (C.ExternalHref "link 3" |> N.navItem "item 3")
+        (List.drop 3 sampleNavItems)
         N.empty
     , N.withNoSelectedItems
         (sampleNavItems
-            |> Array.toList
             |> List.map (N.navItemWithIcon I.unisonMark)
         )
         N.empty
     , N.withNoSelectedItems
         (sampleNavItems
-            |> Array.toList
             |> List.map (N.navItemWithNudge (UI.Nudge.nudge |> UI.Nudge.pulsate))
         )
         N.empty
     , N.withNoSelectedItems
         (sampleNavItems
-            |> Array.toList
             |> List.map (N.navItemWithTooltip (UI.Tooltip.text "tooltip text" |> UI.Tooltip.tooltip))
         )
         N.empty
