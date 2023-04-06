024eb953b50cc58ec30b19e4c087fbe36301a8cb
Simon Højberg <r.hackr@gmail.com>
2023-04-06 13:40:25 -0400

    Update project and branch summary decode
    ProjectSummary
    --------------
    * Decode `owner.handle` expecting a "@" prefix.
    
    BranchSummary
    -------------
    * Decode the project field using `Project.decodeSummary`.
    * Decode `causalHash` expecting a "#" prefix.

--- src/Code/Project.elm
+++ src/Code/Project.elm
@@ -148,7 +148,7 @@ decodeDetails =
             Decode.map isFavedFromBool bool
     in
     Decode.succeed makeProjectDetails
-        |> requiredAt [ "owner", "handle" ] UserHandle.decodeUnprefixed
+        |> requiredAt [ "owner", "handle" ] UserHandle.decode
         |> required "slug" ProjectSlug.decode
         |> required "summary" (nullable string)
         |> required "tags" (Decode.list string)
@@ -171,6 +171,6 @@ decodeSummary =
             }
     in
     Decode.succeed makeProjectSummary
-        |> requiredAt [ "owner", "handle" ] UserHandle.decodeUnprefixed
+        |> requiredAt [ "owner", "handle" ] UserHandle.decode
         |> required "slug" ProjectSlug.decode
         |> required "visibility" decodeVisibility
