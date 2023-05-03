8845cd2ec46fb7ee0fb535594c56cb1996273fd0
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-05-03 18:10:18 +0200

    Remove unused files, add another sample

--- storybook/stories/Stories/Code/code.stories.js
+++ storybook/stories/Stories/Code/code.stories.js
@@ -2,15 +2,10 @@ export default { title: "Code" };
 
 import { initElmStory } from "../../initElmStory.js";
 import WorkspaceItem from "./WorkspaceItem.elm";
-import Readme from "./Readme.elm";
 import Workspace from "./Workspace.elm";
 
 export const workspaceItem = () => {
   return initElmStory(WorkspaceItem.Elm.Stories.Code.WorkspaceItem);
-};
-
-export const readme = () => {
-  return initElmStory(Readme.Elm.Stories.Code.Readme);
 };
 
 export const workspace = () => {
