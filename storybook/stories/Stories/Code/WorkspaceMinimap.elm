bcb0cf60c24ef56f3f8940ce73cc1382f7eb29ae
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-05-19 18:03:19 +0200

    Update Minimap Story to use Minimap model

--- storybook/stories/Stories/Code/WorkspaceMinimap.elm
+++ storybook/stories/Stories/Code/WorkspaceMinimap.elm
@@ -4,11 +4,9 @@ import Browser
 import Code.Definition.Reference as Reference
 import Code.FullyQualifiedName as FQN
 import Code.HashQualified as HashQualified
-import Code.Workspace as Workspace
 import Code.Workspace.WorkspaceItem as WorkspaceItem
 import Code.Workspace.WorkspaceItems as WorkspaceItems
 import Code.Workspace.WorkspaceMinimap as WorkspaceMinimap
-import Dict exposing (Dict, empty, insert, remove)
 import Html exposing (Html)
 import Http
 import Lib.OperatingSystem as OperatingSystem
@@ -16,14 +14,12 @@ import UI.KeyboardShortcut as KeyboardShortcut exposing (KeyboardShortcut(..))
 
 
 type alias Model =
-    { workspaceItemDict : Dict Int WorkspaceItem.WorkspaceItem
-    }
+    WorkspaceMinimap.Model
 
 
 type Msg
-    = WorkspaceMsg Workspace.Msg
+    = WorkspaceMinimapMsg WorkspaceMinimap.Msg
     | GotItem Int Reference.Reference (Result Http.Error WorkspaceItem.Item)
-    | WorkspaceMinimapMsg WorkspaceMinimap.Msg
 
 
 main : Program () Model Msg
@@ -38,7 +34,7 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { workspaceItemDict = Dict.empty }
+    ( WorkspaceMinimap.init (KeyboardShortcut.init OperatingSystem.MacOS) WorkspaceItems.empty
     , Cmd.batch
         [ getSampleResponse 0 "/increment_term_def.json" "increment"
         , getSampleResponse 1 "/nat_gt_term_def.json" "nat_gt"
@@ -69,10 +65,14 @@ getSampleResponse index url termName =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        WorkspaceMsg _ ->
-            ( model, Cmd.none )
+        WorkspaceMinimapMsg mMsg ->
+            let
+                ( updatedModel, cmd ) =
+                    WorkspaceMinimap.update mMsg model
+            in
+            ( updatedModel, Cmd.map WorkspaceMinimapMsg cmd )
 
-        GotItem index reference result ->
+        GotItem _ reference result ->
             case result of
                 Err error ->
                     Debug.log (Debug.toString error)
@@ -80,36 +80,15 @@ update message model =
 
                 Ok item ->
                     let
-                        workspaceItem =
-                            WorkspaceItem.fromItem reference item
-
-                        newDict =
-                            insert index workspaceItem model.workspaceItemDict
+                        newWorkspaceItems =
+                            item
+                                |> WorkspaceItem.fromItem reference
+                                |> WorkspaceItems.prependWithFocus model.workspaceItems
                     in
-                    ( { model | workspaceItemDict = newDict }, Cmd.none )
-
-        WorkspaceMinimapMsg mMsg ->
-            case mMsg of
-                WorkspaceMinimap.SelectItem _ ->
-                    ( model, Cmd.none )
-
-                WorkspaceMinimap.CloseAll ->
-                    ( { workspaceItemDict = empty }, Cmd.none )
+                    ( { model | workspaceItems = newWorkspaceItems }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    case Dict.values model.workspaceItemDict of
-        [] ->
-            Html.text "no items"
-
-        x :: xs ->
-            let
-                workspaceItems =
-                    WorkspaceItems.fromItems [] x xs
-
-                minimapModel =
-                    { keyboardShortcut = KeyboardShortcut.init OperatingSystem.MacOS, workspaceItems = workspaceItems }
-            in
-            WorkspaceMinimap.view minimapModel
-                |> Html.map WorkspaceMinimapMsg
+    WorkspaceMinimap.view model
+        |> Html.map WorkspaceMinimapMsg
