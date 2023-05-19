261c1e370b17914f9ffa1d6ee1cafdc219c39595
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-05-19 18:11:06 +0200

    tmp commit (Workspace story update)

--- storybook/stories/Stories/Code/Workspace.elm
+++ storybook/stories/Stories/Code/Workspace.elm
@@ -2,11 +2,11 @@ module Stories.Code.Workspace exposing (..)
 
 import Browser
 import Code.Definition.Reference as Reference
-import Code.DefinitionSummaryTooltip
+import Code.DefinitionSummaryTooltip as DefinitionSummaryTooltip
 import Code.FullyQualifiedName as FQN
 import Code.HashQualified exposing (HashQualified(..))
 import Code.Syntax exposing (..)
-import Code.Workspace exposing (Msg(..))
+import Code.Workspace as Workspace exposing (Msg(..))
 import Code.Workspace.WorkspaceItem exposing (Item, WorkspaceItem(..), decodeItem, fromItem)
 import Code.Workspace.WorkspaceItems as WorkspaceItems
 import Code.Workspace.WorkspaceMinimap as WorkspaceMinimap
@@ -14,14 +14,18 @@ import Dict exposing (Dict, insert)
 import Helpers.ReferenceHelper exposing (sampleReference)
 import Html exposing (Html)
 import Http
-import Lib.OperatingSystem
-import UI.KeyboardShortcut
+import Lib.OperatingSystem as OperatingSystem
+import UI.KeyboardShortcut as KeyboardShortcut exposing (KeyboardShortcut(..))
 import UI.ViewMode
 
 
 type alias Model =
-    { workspaceItemDict : Dict Int WorkspaceItem
-    }
+    Workspace.Model
+
+
+
+-- { workspaceItemDict : Dict Int WorkspaceItem
+-- }
 
 
 main : Program () Model Message
@@ -36,7 +40,11 @@ main =
 
 init : () -> ( Model, Cmd Message )
 init _ =
-    ( { workspaceItemDict = Dict.empty }
+    ( { workspaceItems = WorkspaceItems.empty
+      , keyboardShortcut = KeyboardShortcut.init OperatingSystem.MacOS
+      , definitionSummaryTooltip = DefinitionSummaryTooltip.init
+      , minimap = WorkspaceMinimap.init (KeyboardShortcut.init OperatingSystem.MacOS) WorkspaceItems.empty
+      }
     , Cmd.batch
         [ getSampleResponse 0 "/increment_term_def.json" "increment"
         , getSampleResponse 1 "/nat_gt_term_def.json" "nat_gt"
@@ -86,6 +94,7 @@ update message model =
                         workspaceItem =
                             fromItem reference item
 
+                        TODO: write some here
                         newDict =
                             insert index workspaceItem model.workspaceItemDict
                     in
@@ -94,23 +103,28 @@ update message model =
 
 view : Model -> Html Message
 view model =
-    case Dict.values model.workspaceItemDict of
-        [] ->
-            Html.text "no items"
+    Workspace.view
+        UI.ViewMode.Regular
+        model
+        |> Html.map WorkspaceMsg
 
-        x :: xs ->
-            let
-                workspaceItems =
-                    WorkspaceItems.fromItems [] x xs
 
-                workspaceModel =
-                    { workspaceItems = workspaceItems
-                    , keyboardShortcut = UI.KeyboardShortcut.init Lib.OperatingSystem.MacOS -- just a sample
-                    , definitionSummaryTooltip = Code.DefinitionSummaryTooltip.init
-                    , minimap = WorkspaceMinimap.init (UI.KeyboardShortcut.init Lib.OperatingSystem.MacOS) workspaceItems
-                    }
-            in
-            Code.Workspace.view
-                UI.ViewMode.Regular
-                workspaceModel
-                |> Html.map WorkspaceMsg
+
+-- case Dict.values model.workspaceItemDict of
+--     [] ->
+--         Html.text "no items"
+--     x :: xs ->
+--         let
+--             workspaceItems =
+--                 WorkspaceItems.fromItems [] x xs
+--             workspaceModel =
+--                 { workspaceItems = workspaceItems
+--                 , keyboardShortcut = UI.KeyboardShortcut.init Lib.OperatingSystem.MacOS -- just a sample
+--                 , definitionSummaryTooltip = Code.DefinitionSummaryTooltip.init
+--                 , minimap = WorkspaceMinimap.init (UI.KeyboardShortcut.init Lib.OperatingSystem.MacOS) workspaceItems
+--                 }
+--         in
+--         Code.Workspace.view
+--             UI.ViewMode.Regular
+--             workspaceModel
+--             |> Html.map WorkspaceMsg
