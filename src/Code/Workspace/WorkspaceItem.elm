ad48c6b4fd774f5ff525b25383feb606b12bd8af
Simon Højberg <r.hackr@gmail.com>
2023-07-05 20:37:20 -0400

    bolder workspace item icon

--- src/Code/Workspace/WorkspaceItem.elm
+++ src/Code/Workspace/WorkspaceItem.elm
@@ -735,7 +735,7 @@ viewClosableRow ref attrs header content foldRow =
                 Icon.arrowsToLine
 
             else
-                Icon.arrowsTopBottomToLine
+                Icon.arrowsFromLine
 
         foldButton fr =
             Button.icon fr.toggle (foldIcon fr.zoom)
