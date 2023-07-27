c2ec8c490e0aa65f3432ad14d13cd2db21fae0c8
Simon Højberg <r.hackr@gmail.com>
2023-07-27 16:20:58 -0400

    Various comparision functions for Sizing

--- src/UI/Sizing.elm
+++ src/UI/Sizing.elm
@@ -5,6 +5,31 @@ type Rem
     = Rem Float
 
 
+lt : Rem -> Rem -> Bool
+lt (Rem a) (Rem b) =
+    a < b
+
+
+le : Rem -> Rem -> Bool
+le (Rem a) (Rem b) =
+    a <= b
+
+
+ge : Rem -> Rem -> Bool
+ge (Rem a) (Rem b) =
+    a >= b
+
+
+gt : Rem -> Rem -> Bool
+gt (Rem a) (Rem b) =
+    a > b
+
+
+equals : Rem -> Rem -> Bool
+equals (Rem a) (Rem b) =
+    a == b
+
+
 fromPx : Int -> Rem
 fromPx px =
     Rem (toFloat oneRemInPx / toFloat px)
