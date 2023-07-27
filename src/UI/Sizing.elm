053c84dec63d9c8d0b9a40ab17dc23ff2db73a27
Simon Højberg <r.hackr@gmail.com>
2023-07-27 16:22:41 -0400

    various sizing helper functions

--- src/UI/Sizing.elm
+++ src/UI/Sizing.elm
@@ -5,18 +5,33 @@ type Rem
     = Rem Float
 
 
+multiply : Rem -> Rem -> Rem
+multiply (Rem a) (Rem b) =
+    Rem (a * b)
+
+
+add : Rem -> Rem -> Rem
+add (Rem a) (Rem b) =
+    Rem (a + b)
+
+
+subtract : Rem -> Rem -> Rem
+subtract (Rem a) (Rem b) =
+    Rem (a - b)
+
+
 lt : Rem -> Rem -> Bool
 lt (Rem a) (Rem b) =
     a < b
 
 
-le : Rem -> Rem -> Bool
-le (Rem a) (Rem b) =
+lteq : Rem -> Rem -> Bool
+lteq (Rem a) (Rem b) =
     a <= b
 
 
-ge : Rem -> Rem -> Bool
-ge (Rem a) (Rem b) =
+gteq : Rem -> Rem -> Bool
+gteq (Rem a) (Rem b) =
     a >= b
 
 
