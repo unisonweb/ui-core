d4b81ee67647acaff992cebd79e55741988538ff
Simon Højberg <r.hackr@gmail.com>
2023-11-28 11:06:59 -0500

    Click outside debugging

--- src/Lib/OnClickOutside.js
+++ src/Lib/OnClickOutside.js
@@ -11,28 +11,41 @@ class OnClickOutside extends HTMLElement {
     this.onMouseDown = (e) => {
       const style = window.getComputedStyle(this);
 
-      console.log("click");
+      const minMd = this.closest(".min-md");
+      const maxMd = this.closest(".max-md");
+
+      let x = "unknown";
+
+      if (minMd) {
+        x = "min-md";
+      }
+
+      if (maxMd) {
+        x = "max-md";
+      }
+      
+      console.log(x, "click");
 
       // If the element isn't visible, there's no point in triggering the
       // clickoutside event. This is useful when multiple variants of a
       // clickoutside is active for the same UI (one for desktop and one for
       // mobile for instance).
       if (style.display === 'none' || this.offsetParent === null) {
+        console.log(x, "is not visible");
         return;
       }
-
 
       const isOutside = !document
         .getElementById("on-click-outside")
         .contains(e.target)
 
       if (isOutside) {
-        console.log("its outside...");
+        console.log(x, "its outside...");
         const event = new CustomEvent("clickOutside");
         this.dispatchEvent(event);
       }
       else {
-        console.log("its NOT outside...");
+        console.log(x, "its NOT outside...");
       }
     };
 
