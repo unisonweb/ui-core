db6e8d52f1cb594053708dfbf9453ed7f153203d
Simon Højberg <r.hackr@gmail.com>
2023-11-30 10:16:12 -0500

    Support rich status banner

--- src/UI/StatusBanner.elm
+++ src/UI/StatusBanner.elm
@@ -5,56 +5,79 @@ import Html.Attributes exposing (class)
 import UI.StatusIndicator as StatusIndicator
 
 
-type StatusBanner
-    = Good String
-    | Bad String
-    | Info String
-    | Working String
+type StatusBanner msg
+    = Good (Html msg)
+    | Bad (Html msg)
+    | Info (Html msg)
+    | Working (Html msg)
 
 
 good : String -> Html msg
 good text_ =
-    view (Good text_)
+    good_ (multilined text_)
+
+
+good_ : Html msg -> Html msg
+good_ content =
+    view (Good content)
 
 
 bad : String -> Html msg
 bad text_ =
-    view (Bad text_)
+    bad_ (multilined text_)
+
+
+bad_ : Html msg -> Html msg
+bad_ content =
+    view (Bad content)
 
 
 info : String -> Html msg
 info text_ =
-    view (Info text_)
+    info_ (multilined text_)
+
+
+info_ : Html msg -> Html msg
+info_ content =
+    view (Info content)
 
 
 working : String -> Html msg
 working text_ =
-    view (Working text_)
+    working_ (multilined text_)
 
 
-view : StatusBanner -> Html msg
+working_ : Html msg -> Html msg
+working_ content =
+    view (Working content)
+
+
+multilined : String -> Html msg
+multilined text_ =
+    text_
+        |> String.split "\n"
+        |> List.map (\t -> p [] [ text t ])
+        |> div []
+
+
+view : StatusBanner msg -> Html msg
 view banner =
     let
-        ( className, indicator, text_ ) =
+        ( className, indicator, content ) =
             case banner of
-                Good t ->
-                    ( "good", StatusIndicator.good, t )
+                Good c ->
+                    ( "good", StatusIndicator.good, c )
 
-                Bad t ->
-                    ( "bad", StatusIndicator.bad, t )
+                Bad c ->
+                    ( "bad", StatusIndicator.bad, c )
 
-                Info t ->
-                    ( "info", StatusIndicator.info, t )
+                Info c ->
+                    ( "info", StatusIndicator.info, c )
 
-                Working t ->
-                    ( "working", StatusIndicator.working, t )
-
-        content =
-            text_
-                |> String.split "\n"
-                |> List.map (\t -> p [] [ text t ])
+                Working c ->
+                    ( "working", StatusIndicator.working, c )
     in
     div [ class ("status-banner status-banner_" ++ className) ]
         [ indicator |> StatusIndicator.view
-        , div [ class "status-banner_content" ] content
+        , div [ class "status-banner_content" ] [ content ]
         ]
