6fa129ec607e07599107aa48848ea16394ba1993
Simon Højberg <r.hackr@gmail.com>
2023-12-05 11:40:49 -0500

    Textarea value

--- src/UI/Form/TextField.elm
+++ src/UI/Form/TextField.elm
@@ -233,7 +233,6 @@ view textField =
 
         input__ =
             if textField.rows > 1 then
-                -- textarea (rows textField.rows :: attrs) [ text textField.value ]
                 textarea (value textField.value :: rows textField.rows :: attrs) []
 
             else
