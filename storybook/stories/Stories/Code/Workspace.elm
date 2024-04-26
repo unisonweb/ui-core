4311ed84b450493044652e464ff24f7e3b0b4ddc
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2024-04-26 16:02:38 +0200

    Update storybook Workspace to use `open` of Workspace for data fetch

--- storybook/stories/Stories/Code/Workspace.elm
+++ storybook/stories/Stories/Code/Workspace.elm
@@ -1,9 +1,9 @@
 module Stories.Code.Workspace exposing (..)
 
 import Browser
+import Code.CodebaseApi as CodebaseApi
 import Code.Config exposing (Config)
 import Code.Definition.Reference as Reference
-import Code.DefinitionSummaryTooltip as DefinitionSummaryTooltip
 import Code.FullyQualifiedName as FQN
 import Code.HashQualified exposing (HashQualified(..))
 import Code.Perspective as Perspective
@@ -11,12 +11,14 @@ import Code.Syntax exposing (..)
 import Code.Workspace as Workspace
 import Code.Workspace.WorkspaceItem exposing (Item, WorkspaceItem(..), decodeItem, fromItem)
 import Code.Workspace.WorkspaceItems as WorkspaceItems
-import Html exposing (Html)
+import Html exposing (Html, div)
+import Html.Events exposing (onClick)
 import Http
 import Lib.HttpApi as HttpApi exposing (ApiUrl(..), Endpoint(..))
 import Lib.OperatingSystem as OperatingSystem
 import UI.KeyboardShortcut as KeyboardShortcut exposing (KeyboardShortcut(..))
 import UI.ViewMode
+import Url
 
 
 type alias Model =
@@ -35,18 +37,35 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { workspaceItems = WorkspaceItems.empty
-      , keyboardShortcut = KeyboardShortcut.init OperatingSystem.MacOS
-      , workspaceItemViewState = Code.Workspace.WorkspaceItem.viewState
-      , isMinimapToggled = False
-      }
-    , Cmd.batch
-        [ getSampleResponse 0 "/increment_term_def.json" "increment"
-        , getSampleResponse 1 "/nat_gt_term_def.json" "nat_gt"
-        , getSampleResponse 2 "/base_readme.json" "base_readme"
-        , getSampleResponse 3 "/long.json" "assets.indexHtml"
-        ]
-    )
+    let
+        reference =
+            "increment"
+                |> FQN.fromString
+                |> NameOnly
+                |> Reference.TypeReference
+
+        model =
+            { workspaceItems = WorkspaceItems.empty
+            , keyboardShortcut = KeyboardShortcut.init OperatingSystem.MacOS
+            , workspaceItemViewState = Code.Workspace.WorkspaceItem.viewState
+            , isMinimapToggled = False
+            }
+
+        openResult =
+            Workspace.open config model reference
+    in
+    Tuple.mapSecond (Cmd.map WorkspaceMsg) openResult
+
+
+
+-- ( model
+-- , Cmd.batch
+--     [ getSampleResponse 0 "/increment_term_def.json" "increment"
+--     , getSampleResponse 1 "/nat_gt_term_def.json" "nat_gt"
+--     , getSampleResponse 2 "/base_readme.json" "base_readme"
+--     , getSampleResponse 3 "/long.json" "assets.indexHtml"
+--     ]
+-- )
 
 
 getSampleResponse : Int -> String -> String -> Cmd Msg
@@ -71,16 +90,47 @@ getSampleResponse index url termName =
 type Msg
     = WorkspaceMsg Workspace.Msg
     | GotItem Int Reference.Reference (Result Http.Error Item)
+    | OpenNext
 
 
 codebaseHash : Endpoint
 codebaseHash =
-    GET { path = [ "list" ], queryParams = [] }
+    GET { path = [ "" ], queryParams = [] }
+
+
+refToEndpoint : Reference.Reference -> Endpoint
+refToEndpoint ref =
+    let
+        refStringToMockFileName : String -> String
+        refStringToMockFileName input =
+            case input of
+                "increment" ->
+                    "increment_term_def.json"
+
+                "nat_gt" ->
+                    "/nat_gt_term_def.json"
+
+                "base_readme" ->
+                    "/base_readme.json"
+
+                "assets.indexHtml" ->
+                    "/long.json"
+
+                _ ->
+                    ""
+    in
+    GET
+        { path = [ refStringToMockFileName (Reference.toApiUrlString ref) ]
+        , queryParams = []
+        }
 
 
 api : HttpApi.HttpApi
 api =
-    { url = SameOrigin []
+    { url =
+        Url.fromString "http://localhost:6006"
+            |> Maybe.map CrossOrigin
+            |> Maybe.withDefault (SameOrigin [])
     , headers = []
     }
 
@@ -89,7 +139,20 @@ config : Config
 config =
     { operatingSystem = OperatingSystem.MacOS
     , perspective = Perspective.relativeRootPerspective
-    , toApiEndpoint = \_ -> codebaseHash
+    , toApiEndpoint =
+        \endpoint ->
+            case endpoint of
+                CodebaseApi.Find _ ->
+                    codebaseHash
+
+                CodebaseApi.Browse _ ->
+                    codebaseHash
+
+                CodebaseApi.Definition { ref } ->
+                    refToEndpoint ref
+
+                CodebaseApi.Summary _ ->
+                    codebaseHash
     , api = api
     }
 
@@ -107,7 +170,7 @@ update message model =
         GotItem _ reference result ->
             case result of
                 Err error ->
-                        ( model, Cmd.none )
+                    ( model, Cmd.none )
 
                 Ok item ->
                     let
@@ -121,10 +184,28 @@ update message model =
                     in
                     ( newModel, Cmd.none )
 
+        OpenNext ->
+            let
+                reference =
+                    "assets.indexHtml"
+                        |> FQN.fromString
+                        |> NameOnly
+                        |> Reference.TypeReference
+
+                openResult =
+                    Workspace.open config model reference
+            in
+            ( Tuple.first openResult
+            , Tuple.second openResult |> Cmd.map WorkspaceMsg
+            )
+
 
 view : Model -> Html Msg
 view model =
-    Workspace.view
-        UI.ViewMode.Regular
-        model
-        |> Html.map WorkspaceMsg
+    div []
+        [ Workspace.view
+            UI.ViewMode.Regular
+            model
+            |> Html.map WorkspaceMsg
+        , Html.button [ onClick OpenNext ] [ Html.text "Open next" ]
+        ]
