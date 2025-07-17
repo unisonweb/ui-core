6494d210221949e3cf5b93d40d371d7647d04d71
Simon Højberg <r.hackr@gmail.com>
2025-07-17 14:05:43 -0400

    Monitor attribute changes to ProdDebug message

--- src/Lib/ProdDebug.js
+++ src/Lib/ProdDebug.js
@@ -1,11 +1,23 @@
 // <prod-debug message="debug message"></prod-debug>
 
 class ProdDebug extends HTMLElement {
+  static get observedAttributes() {
+    return ["message"];
+  }
+
   constructor() {
     super();
   }
 
-  async connectedCallback() {
+  connectedCallback() {
+    this.log();
+  }
+
+  attributeChangedCallback() {
+    this.log();
+  }
+
+  log() {
     const message = this.getAttribute("message");
     console.debug(message);
   }
