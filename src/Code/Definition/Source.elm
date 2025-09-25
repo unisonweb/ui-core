c67fd70ac1271bc98515287b9000b868365e0ac1
Simon Højberg <r.hackr@gmail.com>
2025-09-25 15:34:09 -0400

    Remove unused import

--- src/Code/Definition/Source.elm
+++ src/Code/Definition/Source.elm
@@ -16,7 +16,7 @@ import Code.Definition.Type as Type exposing (TypeSource)
 import Code.FullyQualifiedName as FQN exposing (FQN)
 import Code.Source.SourceViewConfig as SourceViewConfig exposing (SourceViewConfig)
 import Code.Syntax as Syntax
-import Html exposing (Html, div, pre, span, text)
+import Html exposing (Html, div, span, text)
 import Html.Attributes exposing (class)
 import UI
 
