169a5f9f15451bfea5819a9436cf4b038eb5e630
Simon Højberg <r.hackr@gmail.com>
2025-11-07 12:25:14 -0500

    Add UI.ButtonGroup

---
+++ src/UI/ButtonGroup.elm
@@ -0,0 +1,60 @@
+module UI.ButtonGroup exposing (..)
+
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
+import UI.Button as Button exposing (Button)
+
+
+type Direction
+    = Horizontal
+    | Vertical
+
+
+type alias ButtonGroup msg =
+    { buttons : List (Button msg)
+    , direction : Direction
+    }
+
+
+empty : ButtonGroup msg
+empty =
+    { buttons = [], direction = Horizontal }
+
+
+buttonGroup : List (Button msg) -> ButtonGroup msg
+buttonGroup buttons =
+    { buttons = buttons, direction = Horizontal }
+
+
+
+-- MODIFY
+
+
+horizontal : ButtonGroup msg -> ButtonGroup msg
+horizontal group =
+    { group | direction = Horizontal }
+
+
+vertical : ButtonGroup msg -> ButtonGroup msg
+vertical group =
+    { group | direction = Vertical }
+
+
+add : Button msg -> ButtonGroup msg -> ButtonGroup msg
+add button group =
+    add_ [ button ] group
+
+
+add_ : List (Button msg) -> ButtonGroup msg -> ButtonGroup msg
+add_ buttons group =
+    { group | buttons = group.buttons ++ buttons }
+
+
+
+-- VIEW
+
+
+view : ButtonGroup msg -> Html msg
+view group =
+    div [ class "button-group" ]
+        (List.map Button.view group.buttons)
