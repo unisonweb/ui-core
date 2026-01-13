6aae89092f86e791ce1bf8b4c2485d6fa0989498
Simon Højberg <r.hackr@gmail.com>
2026-01-13 10:12:44 -0500

    Add ScrollTo.scrollToTop + ScrollTo.scrollToBottom

--- src/Lib/ScrollTo.elm
+++ src/Lib/ScrollTo.elm
@@ -4,6 +4,25 @@ import Browser.Dom as Dom
 import Task
 
 
+scrollToBottom : msg -> String -> Cmd msg
+scrollToBottom doneMsg containerId =
+    Dom.getElement containerId
+        |> Task.map (.element >> .height)
+        |> Task.andThen
+            (\height ->
+                Dom.setViewportOf containerId 0 height
+                    |> Task.onError (\_ -> Task.succeed ())
+            )
+        |> Task.attempt (always doneMsg)
+
+
+scrollToTop : msg -> String -> Cmd msg
+scrollToTop doneMsg containerId =
+    Dom.setViewportOf containerId 0 0
+        |> Task.onError (\_ -> Task.succeed ())
+        |> Task.attempt (always doneMsg)
+
+
 scrollTo : msg -> String -> String -> Cmd msg
 scrollTo doneMsg containerId targetId =
     scrollTo_ doneMsg containerId targetId 0
